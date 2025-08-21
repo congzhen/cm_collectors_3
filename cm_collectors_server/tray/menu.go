5b18266fbc1ea52649a171441f6ce8d8c9f98cf2
congzhen <congzhen17173@163.com>
2025-08-21 22:43:44 +0800

    feat(tray): 添加打开应用程序菜单项并实现启动wails应用功能
    - 在托盘菜单中添加"打开应用程序"选项
    - 实现启动wails应用的功能，包括获取可执行文件路径、构建wails应用路径和服务器地址
    - 添加Content-Security-Policy到wails前端页面的head标签中

--- cm_collectors_server/tray/menu.go
+++ cm_collectors_server/tray/menu.go
@@ -2,6 +2,9 @@ package tray
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/getlantern/systray"
@@ -10,6 +13,7 @@ import (
 // TrayMenu 托盘菜单结构
 type TrayMenu struct {
 	OpenWebUI *systray.MenuItem
+	OpenApp   *systray.MenuItem
 	Quit      *systray.MenuItem
 }
 
@@ -29,6 +33,7 @@ func CreateTrayMenu(iconData []byte, serverAddr string) *TrayMenu {
 	// 创建菜单项
 	menu := &TrayMenu{
 		OpenWebUI: systray.AddMenuItem("打开网页界面", "在浏览器中打开管理界面"),
+		OpenApp:   systray.AddMenuItem("打开应用程序", "启动桌面应用程序"),
 		Quit:      systray.AddMenuItem("退出", "停止服务器并退出程序"),
 	}
 
@@ -46,6 +51,32 @@ func (tm *TrayMenu) HandleEvents(shutdownFunc func()) {
 				// 将0.0.0.0转换为localhost并添加端口
 				displayAddr := strings.Replace(ServerAddr, "0.0.0.0", "localhost", 1)
 				OpenBrowser(fmt.Sprintf("http://%s", displayAddr))
+			case <-tm.OpenApp.ClickedCh:
+				// 获取当前可执行文件的路径
+				execPath, err := os.Executable()
+				if err != nil {
+					fmt.Printf("无法获取可执行文件路径: %v\n", err)
+					break
+				}
+
+				// 构建wails应用的路径（与当前程序在同一目录）
+				dir := filepath.Dir(execPath)
+				wailsApp := filepath.Join(dir, "cm_collectors_wails.exe")
+
+				// 将0.0.0.0转换为127.0.0.1
+				appServerAddr := strings.Replace(ServerAddr, "0.0.0.0", "127.0.0.1", 1)
+				// 确保URL有http://前缀
+				if !strings.HasPrefix(appServerAddr, "http://") && !strings.HasPrefix(appServerAddr, "https://") {
+					appServerAddr = "http://" + appServerAddr
+				}
+
+				// 启动wails应用并传递服务器地址参数
+				cmd := exec.Command(wailsApp, fmt.Sprintf("-url=%s", appServerAddr))
+				if err := cmd.Start(); err != nil {
+					fmt.Printf("无法启动应用程序: %v\n", err)
+				} else {
+					fmt.Println("应用程序已启动-url=", appServerAddr)
+				}
 			case <-tm.Quit.ClickedCh:
 				fmt.Println("正在退出...")
 				// 调用关闭函数
