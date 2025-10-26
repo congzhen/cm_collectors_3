f71a4125d31ba871b03e040354a0da1020f4fbcf
congzhen <congzhen17173@163.com>
2025-10-27 07:29:00 +0800

    feat(detailsTags): 点击详情中的标签可设置搜索条件
    为 detailsTags 组件中的标签添加点击事件，点击后调用 setSearchHandle 方法，
    将标签信息传递给搜索状态管理器以设置查询条件。同时为标签添加指针样式
    和禁止文本选中样式，提升用户体验。

--- cm_collectors_html/src/components/details/detailsTags.vue
+++ cm_collectors_html/src/components/details/detailsTags.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="details-tag-list">
-    <el-tag type="info" effect="plain" size="large" v-for="item, key in tags_C" :key="key">
+    <el-tag type="info" effect="plain" size="large" v-for="item, key in tags_C" :key="key"
+      @click="setSearchHandle(item)">
       {{ item.name }}
     </el-tag>
   </div>
 </template>
 <script lang="ts" setup>
+import { E_tagType } from '@/dataType/app.dataType';
 import type { I_resource } from '@/dataType/resource.dataType';
+import type { I_tag } from '@/dataType/tag.dataType';
+import { searchStoreData } from '@/storeData/search.storeData';
 import { computed, type PropType } from 'vue';
+const store = {
+  searchStoreData: searchStoreData(),
+}
 const props = defineProps({
   resource: {
     type: Object as PropType<I_resource>,
@@ -19,6 +26,11 @@ const tags_C = computed(() => {
   return props.resource.tags
 })
 
+const setSearchHandle = (tag: I_tag) => {
+  console.log(tag);
+  store.searchStoreData.setQuery(E_tagType.DiyTag, tag.id, tag.tagClass_id);
+}
+
 </script>
 <style lang="scss" scoped>
 .details-tag-list {
@@ -26,5 +38,10 @@ const tags_C = computed(() => {
   display: flex;
   flex-wrap: wrap;
   gap: 5px;
+
+  .el-tag {
+    cursor: pointer;
+    user-select: none;
+  }
 }
 </style>
